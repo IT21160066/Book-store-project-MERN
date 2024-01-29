0a1f732c5e8092104d474bf4a00fe6a1ae5aceaa
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-01-29 20:24:19 +0530

    userschema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,24 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  activeStatus: {
+    type: Boolean,
+    default: true, //any new user automatically be active
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
