861b274e87760f5f9a7d6e506543f080ff2606d0
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-01-30 09:05:34 +0530

    routes, usercontroller

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,42 @@
+const User = require("../models/User");
+const User = require("../models/Note");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt"); //hash the password before we save it
+const { use } = require("../routes/userRoutes");
+
+// @desc Get all users
+// @route GET/users
+// @access Private
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select("-password").lean();
+  if (!users) {
+    return res.status(400).json({ message: "No users found" });
+  }
+  res.json(users);
+});
+
+// @desc Create new user
+// @route POST/users
+// @access Private
+
+const createNewUser = asyncHandler(async (req, res) => {});
+
+// @desc Update a user
+// @route PATCH/users
+// @access Private
+
+const updateUser = asyncHandler(async (req, res) => {});
+
+// @desc Delete a user
+// @route DELETE/users
+// @access Private
+
+const deleteUser = asyncHandler(async (req, res) => {});
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+};
