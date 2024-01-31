be951421d98e0eb57e898b94b6e7dd45b794a3da
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-01-31 22:22:57 +0530

    features, routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
 import Dashlayout from "./components/Dashlayout";
+import Welcome from "./features/auth/Welcome";
+import NoteList from "./features/notes/NoteList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
@@ -11,7 +14,16 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route path="dash" element={<Dashlayout />}></Route>
+        <Route path="dash" element={<Dashlayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NoteList />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+        </Route>
+        {/* End dash*/}
       </Route>
     </Routes>
   );
